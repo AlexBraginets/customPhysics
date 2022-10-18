c62d253c542afc9592d4b789bd4af7f7527cdb33
AlexBraginets <qetting@gmail.com>
2022-10-18 23:08:09 +0300

    decal shader wip

---
+++ Assets/Scripts/DecalRelated/DecalHandler.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Linq;
+using UnityEngine;
+
+namespace DecalRelated
+{
+    [ExecuteAlways]
+    public class DecalHandler : MonoBehaviour
+    {
+        [SerializeField] private Vector2[] points;
+        [SerializeField] private MeshRenderer renderer;
+
+        [ContextMenu("update material")]
+        private void UpdateMaterial()
+        {
+            renderer.material.SetColorArray("_Points", GetColorArray());
+            renderer.material.SetInt("_PointsCount", points.Length);
+        }
+
+        private Color[] GetColorArray()
+        {
+            var colorArray = new Color[10];
+            var pointArray = points.Select(PointToColor).Take(Mathf.Min(points.Length, 10)).ToArray();
+            int count = pointArray.Length;
+            Array.Copy(pointArray, colorArray, pointArray.Length);
+            return colorArray;
+
+        }
+        private void Update()
+        {
+            UpdateMaterial();
+        }
+
+        private Color PointToColor(Vector2 point)
+        {
+            return new Color(point.x, point.y, 0f);
+        }
+    }
+}
